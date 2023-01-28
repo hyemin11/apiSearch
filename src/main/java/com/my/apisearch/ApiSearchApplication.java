c371e9103c7aa742188a9922b486a8c7b4a0710c
hyemin <01clipper@gmail.com>
2023-01-28 15:50:42 +0900

    initial commit

---
+++ src/main/java/com/my/apisearch/ApiSearchApplication.java
@@ -0,0 +1,13 @@
+package com.my.apisearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/apisearch/ApiSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.apisearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
